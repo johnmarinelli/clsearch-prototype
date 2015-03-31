e7c3a095bb69529991ed34a1a8398db18e55d5e4
John Marinelli <jonmarinelli@hotmail.com>
2015-03-30 20:46:34 -0700

    mailgun

--- app/mailers/thanks_for_registering_mailer.rb
+++ app/mailers/thanks_for_registering_mailer.rb
@@ -1,5 +1,5 @@
 class ThanksForRegisteringMailer < ApplicationMailer
-  default from: ENV['mailgun_username']
+  default from: "mailer@johnmarinelli.me"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
