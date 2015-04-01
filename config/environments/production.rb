833ad5c3ef572feecef2c220e9490132c5de7317
John Marinelli <jonmarinelli@hotmail.com>
2015-04-01 03:10:53 -0700

    mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'immense-falls-2574.herokuapp.com'
+    :domain         => 'immense-falls-2574.herokuapp.com',
     :authentication => :plain,
   }
 end
