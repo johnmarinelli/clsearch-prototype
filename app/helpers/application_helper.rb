f005c7a8efca2f3b5d6589bba046439a8cc8b3e7
John Marinelli <jonmarinelli@hotmail.com>
2015-06-05 21:15:21 -0700

    style query page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def get_navbar_path
+    if user_signed_in?
+      link_to 'Home', root_path
+    else
+      link_to 'Login', login_path
+    end
+  end
+
+  def insert_top_margin
+    if not current_page?(root_path) or user_signed_in?
+      render 'templates/top_margin'
+    end
+  end
 end
