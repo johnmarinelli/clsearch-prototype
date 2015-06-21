63ceaaa7cef3bd6e2f60d46054ada05e43a600c4
John Marinelli <jonmarinelli@hotmail.com>
2015-06-21 14:10:59 -0700

    registration cucumber

--- features/support/registration/registration_expectations.rb
+++ features/support/registration/registration_expectations.rb
@@ -5,9 +5,9 @@ module Registration
 
   class RegistrationExpectations < Common::Expectations
     def has_confirmed_message_in_popup
-      # TODO
-      #raise Exception unless Common::session.has_content? 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
-      raise Exception
+      elem = Common::session.find '#flash-alert'
+      str = /email has been sent to your address/
+      raise Exception if (str =~ elem.text).nil?
     end
   end
 
